9806ba0c157f6394a2b0a3139dde4f1667a10940
Kotagiri lokesh <ktss.kotagiri@gmail.com>
2024-08-17 21:12:33 +0530

    Countryportal

---
+++ CountryPortel/Controllers/CountryController.cs
@@ -0,0 +1,57 @@
+﻿using CountryPortel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryPortel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : Controller
+    {
+        public readonly TrainingDBContext trainingDBContext;
+        public CountryController(TrainingDBContext _trainingDBContext)
+        {
+            trainingDBContext = _trainingDBContext;
+        }
+        [HttpGet("GetCountryDetails")]
+        public List<Country> GetCountryDetails()
+        {
+            List<Country> lstCountry = new List<Country>();
+            try
+            {
+                lstCountry = trainingDBContext.Country.ToList();
+                return lstCountry;
+            }
+            catch (Exception ex)
+            {
+                lstCountry = new List<Country>();
+                return lstCountry;
+            }
+        }
+        [HttpPost("AddCountry")]
+        public string AddCountry(Country country)
+        {
+            string message = string.Empty;
+            try
+            {
+                if (!string.IsNullOrEmpty(country.CountryName))
+                {
+                    trainingDBContext.Add(country);
+                    trainingDBContext.SaveChanges();
+                    message = "Country added successfully";
+                }
+                else
+                    message = "Country Name required.";
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            return message;
+        }
+    }
+}
