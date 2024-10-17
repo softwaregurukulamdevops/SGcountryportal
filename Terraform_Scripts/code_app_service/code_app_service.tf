52fef0b199478201a001d43280f6a058401e026f
softwaregurukulamdevops <pramodkotagiri@softwaregurukulam.com>
2024-10-18 01:44:32 +0530

    Update code_app_service.tf

--- Terraform_Scripts/code_app_service/code_app_service.tf
+++ Terraform_Scripts/code_app_service/code_app_service.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 # Define the Resource Group
 resource "azurerm_resource_group" "example" {
   name     = "rg-countryportal-resources"
-  location = "Brazil US"  # Replace with your desired location
+  location = "UAE Central"  # Replace with your desired location
 }
 
 # Define the Standard App Service Plan
